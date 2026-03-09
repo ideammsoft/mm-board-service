87a7e1a5672286e285da763a1431e35a20f11803
ideakslee <ideakslee@gmail.com>
2026-03-09 15:41:37 +0900

    first commit

---
+++ src/main/java/co/kr/mmsoft/mmboardservice/MmBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package co.kr.mmsoft.mmboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmBoardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmBoardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/kr/mmsoft/mmboardservice/MmBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.mmsoft.mmboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmBoardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
